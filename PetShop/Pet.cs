0d216976774470e1faa162f0d095fd8d4c1504cc
manuxx <manus@pg.gda.pl>
2022-12-01 13:38:13 +0100

    PRZENIESIENIE CRYTERIOW DO KLASY pET

--- PetShop/Pet.cs
+++ PetShop/Pet.cs
@@ -63,51 +63,53 @@ namespace Training.DomainClasses
         public static Predicate<Pet> IsNotSpeciesOf(Species species)
         {
             return pet => pet.species!= species;
+        } 
+        
+        public class BornAfterCriteria : ICriteria<Pet>
+        {
+            private readonly int _year;
+
+            public BornAfterCriteria(int year)
+            {
+                _year = year;
+            }
+
+            public bool IsSatisfiedBy(Pet pet)
+            {
+                return pet.yearOfBirth > _year;
+            }
+        }
+
+        public class SexCriteria : ICriteria<Pet>
+        {
+            private readonly Sex _sex;
+
+            public SexCriteria(Sex sex)
+            {
+                _sex = sex;
+            }
+
+            public bool IsSatisfiedBy(Pet pet)
+            {
+                return pet.sex == _sex;
+            }
+        }
+
+        public class SpeciesCriteria : ICriteria<Pet>
+        {
+            private readonly Species _species;
+
+            public SpeciesCriteria(Species species)
+            {
+                _species = species;
+            }
+
+            public bool IsSatisfiedBy(Pet item)
+            {
+                return item.species == _species;
+            }
         }
     }
 
-    public class BornAfterCriteria : ICriteria<Pet>
-    {
-        private readonly int _year;
-
-        public BornAfterCriteria(int year)
-        {
-            _year = year;
-        }
-
-        public bool IsSatisfiedBy(Pet pet)
-        {
-            return pet.yearOfBirth > _year;
-        }
-    }
-
-    public class SexCriteria : ICriteria<Pet>
-    {
-        private readonly Sex _sex;
-
-        public SexCriteria(Sex sex)
-        {
-            _sex = sex;
-        }
-
-        public bool IsSatisfiedBy(Pet pet)
-        {
-            return pet.sex == _sex;
-        }
-    }
-
-    public class SpeciesCriteria : ICriteria<Pet>
-    {
-        private readonly Species _species;
-
-        public SpeciesCriteria(Species species)
-        {
-            _species = species;
-        }
-
-        public bool IsSatisfiedBy(Pet item)
-        {
-            return item.species == _species;
-        }
-    }
+    
 }
