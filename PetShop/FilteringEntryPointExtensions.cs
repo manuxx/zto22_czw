98a0a7c09c88d4bb5ec9235f727cfac76346928f
manuxx <manus@pg.gda.pl>
2022-12-08 14:51:07 +0100

    ladniejsze negowanie + wyrefaktoryzowana funkcja ApplyNagating

--- PetShop/FilteringEntryPointExtensions.cs
+++ PetShop/FilteringEntryPointExtensions.cs
@@ -1,16 +1,28 @@
 using System;
+using Training.DomainClasses;
 using Training.Specificaton;
 
 public static class FilteringEntryPointExtensions
 {
     public static ICriteria<TItem> EqualTo<TItem, TProperty>(this FilteringEntryPoint<TItem, TProperty> filteringEntryPoint, TProperty species)
     {
-        return new AnonymousCriteria<TItem>(item=> filteringEntryPoint._selector(item).Equals(species) != filteringEntryPoint._negating);
+        ICriteria<TItem> resultCriteria = new AnonymousCriteria<TItem>(item=> filteringEntryPoint._selector(item).Equals(species) );
+        return AplyNegating(filteringEntryPoint, resultCriteria);
+    }
+
+    private static ICriteria<TItem> AplyNegating<TItem, TProperty>(FilteringEntryPoint<TItem, TProperty> filteringEntryPoint,
+        ICriteria<TItem> resultCriteria)
+    {
+        if (filteringEntryPoint._negating)
+            return new Negation<TItem>(resultCriteria);
+        else
+            return resultCriteria;
     }
 
     public static ICriteria<TItem> GreaterThan<TItem, TProperty>(this FilteringEntryPoint<TItem, TProperty> filteringEntryPoint, TProperty value) 
         where TProperty : IComparable<TProperty>
     {
-        return new AnonymousCriteria<TItem>(item => filteringEntryPoint._selector(item).CompareTo(value) > 0);
+        var resultCriteria = new AnonymousCriteria<TItem>(item => filteringEntryPoint._selector(item).CompareTo(value) > 0);
+        return AplyNegating(filteringEntryPoint, resultCriteria);
     }
 }
