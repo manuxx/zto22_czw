4f14e3ca9dd8eb76b04e6198f66741dfe994caa7
manuxx <manus@pg.gda.pl>
2022-12-08 14:12:58 +0100

    teraz podejscie z IComparable dziala bez hackow

--- PetShopSpec/BuilderExtensions.cs
+++ PetShopSpec/BuilderExtensions.cs
@@ -8,10 +8,9 @@ static internal class BuilderExtensions
         return new AnonymousCriteria<TItem>(pet=> criteriaBuilder._selector(pet).Equals(species));
     }
 
-    public static ICriteria<TItem> GreaterThan<TComparablePropert, TItem, TProperty>(this CriteriaBuilder<TItem, TProperty> criteriaBuilder, TComparablePropert value) 
-        where TComparablePropert : IComparable<TProperty>
-
+    public static ICriteria<TItem> GreaterThan<TItem, TProperty>(this CriteriaBuilder<TItem, TProperty> criteriaBuilder, TProperty value) 
+        where TProperty : IComparable<TProperty>
     {
-        return new AnonymousCriteria<TItem>(pet => value.CompareTo(criteriaBuilder._selector(pet)) < 0);
+        return new AnonymousCriteria<TItem>(pet => criteriaBuilder._selector(pet).CompareTo(value) > 0);
     }
 }
