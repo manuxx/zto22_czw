1f445b287bd174ca214b1f25e9df7d7c8e90ff6e
manuxx <manus@pg.gda.pl>
2022-12-08 14:25:48 +0100

    dziala ale troche trzeba poprawic

--- PetShop/FilteringEntryPoint.cs
+++ PetShop/FilteringEntryPoint.cs
@@ -5,10 +5,17 @@ namespace Training.Specificaton
     public class FilteringEntryPoint<TItem,TProperty> 
     {
         public readonly Func<TItem, TProperty> _selector;
+        public bool _negting;
 
         public FilteringEntryPoint(Func<TItem, TProperty> selector)
         {
             _selector = selector;
+        }
+
+        public FilteringEntryPoint<TItem,TProperty> Not()
+        {
+            _negting = true;
+            return this;
         }
     }
 }
--- PetShop/FilteringEntryPointExtensions.cs
+++ PetShop/FilteringEntryPointExtensions.cs
@@ -5,7 +5,7 @@ public static class FilteringEntryPointExtensions
 {
     public static ICriteria<TItem> EqualTo<TItem, TProperty>(this FilteringEntryPoint<TItem, TProperty> filteringEntryPoint, TProperty species)
     {
-        return new AnonymousCriteria<TItem>(item=> filteringEntryPoint._selector(item).Equals(species));
+        return new AnonymousCriteria<TItem>(item=> filteringEntryPoint._selector(item).Equals(species) != filteringEntryPoint._negting);
     }
 
     public static ICriteria<TItem> GreaterThan<TItem, TProperty>(this FilteringEntryPoint<TItem, TProperty> filteringEntryPoint, TProperty value) 
