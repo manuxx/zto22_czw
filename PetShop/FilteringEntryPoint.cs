d1039249ff6f3ee31a924db9870d789f59e02bb3
manuxx <manus@pg.gda.pl>
2022-12-08 14:58:37 +0100

    przeniesienie metody ApplyNegatin do FEP + likwidacja static

--- PetShop/FilteringEntryPoint.cs
+++ PetShop/FilteringEntryPoint.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using Training.DomainClasses;
 
 namespace Training.Specificaton
 {
     public class FilteringEntryPoint<TItem,TProperty> 
     {
         public readonly Func<TItem, TProperty> _selector;
-        public readonly bool _negating;
+        private readonly bool _negating;
 
         public FilteringEntryPoint(Func<TItem, TProperty> selector) : this(selector, false) 
         {
@@ -17,10 +18,18 @@ namespace Training.Specificaton
             _negating = negating;
         }
 
-        public FilteringEntryPoint<TItem,TProperty> Not()
+        public FilteringEntryPoint<TItem, TProperty> Not()
         {
 
             return new FilteringEntryPoint<TItem, TProperty>(_selector, !_negating);
+        }
+
+        public ICriteria<TItem> ApplyNegating(ICriteria<TItem> resultCriteria)
+        {
+            if (_negating)
+                return new Negation<TItem>(resultCriteria);
+            else
+                return resultCriteria;
         }
     }
 }
--- PetShop/FilteringEntryPointExtensions.cs
+++ PetShop/FilteringEntryPointExtensions.cs
@@ -7,22 +7,13 @@ public static class FilteringEntryPointExtensions
     public static ICriteria<TItem> EqualTo<TItem, TProperty>(this FilteringEntryPoint<TItem, TProperty> filteringEntryPoint, TProperty species)
     {
         ICriteria<TItem> resultCriteria = new AnonymousCriteria<TItem>(item=> filteringEntryPoint._selector(item).Equals(species) );
-        return AplyNegating(filteringEntryPoint, resultCriteria);
-    }
-
-    private static ICriteria<TItem> AplyNegating<TItem, TProperty>(FilteringEntryPoint<TItem, TProperty> filteringEntryPoint,
-        ICriteria<TItem> resultCriteria)
-    {
-        if (filteringEntryPoint._negating)
-            return new Negation<TItem>(resultCriteria);
-        else
-            return resultCriteria;
+        return filteringEntryPoint.ApplyNegating(resultCriteria);
     }
 
     public static ICriteria<TItem> GreaterThan<TItem, TProperty>(this FilteringEntryPoint<TItem, TProperty> filteringEntryPoint, TProperty value) 
         where TProperty : IComparable<TProperty>
     {
         var resultCriteria = new AnonymousCriteria<TItem>(item => filteringEntryPoint._selector(item).CompareTo(value) > 0);
-        return AplyNegating(filteringEntryPoint, resultCriteria);
+        return filteringEntryPoint.ApplyNegating(resultCriteria);
     }
 }
